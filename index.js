46334de83ef31b31cf227b81b9c84847a733fa75
github username <github email address>
2019-03-13 17:10:16 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+let cards = ['queen of hearts', 'jack of clubs', 'ten of diamonds', 'ace of spades'];
+
+console.log(cards.indexOf('jack of clubs')); //1
